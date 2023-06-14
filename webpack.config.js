0b2c415312d37019ebd3517cda3b070430d64b6e
Andy <and.razumov2014@gmail.com>
2023-06-14 17:57:00 +0800

    pulled out external dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require("path");
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const dotenv = require('dotenv');
-const webpack = require('webpack');
+const dotenv = require("dotenv");
+const webpack = require("webpack");
 
 module.exports = (webpackConfigEnv, argv) => {
   dotenv.config();
   const importMapUrl = process.env.IMPORT_MAP_URL;
   const isLocal = webpackConfigEnv && webpackConfigEnv.isLocal;
-  const env = isLocal ? 'dev' : 'prod';
+  const env = isLocal ? "dev" : "prod";
 
   const orgName = "andy-inc";
   const defaultConfig = singleSpaDefaults({
@@ -18,41 +18,42 @@ module.exports = (webpackConfigEnv, argv) => {
     webpackConfigEnv,
     argv,
     disableHtmlGeneration: true,
-    orgPackagesAsExternal: false
+    orgPackagesAsExternal: false,
   });
 
   return merge(defaultConfig, {
     entry: {
-      index: path.resolve(__dirname, 'src', 'index.ts'),
+      index: path.resolve(__dirname, "src", "index.ts"),
     },
     output: {
-      publicPath: '/',
-      filename: '[name].js',
-      path: path.resolve(__dirname, 'dist'),
+      publicPath: "/",
+      filename: "[name].js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        inject: 'body',
-        template: path.resolve(__dirname, 'public', 'index.ejs'),
-        publicPath: './',
-        scriptLoading: 'blocking',
+        inject: "body",
+        template: path.resolve(__dirname, "public", "index.ejs"),
+        publicPath: "./",
+        scriptLoading: "blocking",
         templateParameters: {
-          title: 'Микрофронтенды',
+          title: "Микрофронтенды",
           isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
           // Три нижеидущих массива предназначены для регулировки Content Security Policy - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
-          scriptTrustedDomains: ['https:', 'localhost:*', 'http://94.250.250.29:*'],
-          styleSheetTrustedDomains: ['https:', 'localhost:*', 'http://94.250.250.29:*'],
-          connectionTrustedDomains: ['https:', 'localhost:*', 'http://94.250.250.29:*', 'ws://localhost:*'],
+          scriptTrustedDomains: ["https:", "localhost:*", "http://94.250.250.29:*"],
+          styleSheetTrustedDomains: ["https:", "localhost:*", "http://94.250.250.29:*"],
+          connectionTrustedDomains: ["https:", "localhost:*", "http://94.250.250.29:*", "ws://localhost:*"],
           importMapUrl: `${importMapUrl}${env}`,
           orgName,
         },
       }),
       new webpack.DefinePlugin({
-        API_URL: isLocal ? JSON.stringify('http://localhost') : JSON.stringify('http://94.250.250.29')
-      })
+        API_URL: isLocal ? JSON.stringify("http://localhost") : JSON.stringify("http://94.250.250.29"),
+      }),
     ],
     devServer: {
-      historyApiFallback: true
-    }
+      historyApiFallback: true,
+    },
+    externals: ["axios"],
   });
 };
